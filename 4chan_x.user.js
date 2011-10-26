2b754b740e49aad4f549e3864e2cdb3e00aad89d
Nicolas Stepien <mayhemydg@gmail.com>
2011-10-27 01:40:59 +0200

    cp

--- 4chan_x.user.js
+++ 4chan_x.user.js
@@ -2548,7 +2548,9 @@
   redirect = function() {
     var url;
     switch (g.BOARD) {
+      case 'diy':
       case 'g':
+      case 'pol':
       case 'sci':
         url = "http://archive.installgentoo.net/cgi-board.pl/" + g.BOARD + "/thread/" + g.THREAD_ID;
         break;
